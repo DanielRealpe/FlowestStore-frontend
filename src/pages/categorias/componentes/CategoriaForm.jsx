5ba81bbeda5ceabca49e41098964cc9882587d25
JustAHarmlessAle <103162016+JustAHarmlessAle@users.noreply.github.com>
2025-08-24 23:16:42 -0500

    Update CategoriaForm.jsx

--- src/pages/categorias/componentes/CategoriaForm.jsx
+++ src/pages/categorias/componentes/CategoriaForm.jsx
@@ -56,7 +56,7 @@ const CategoriaForm = ({ categoria, onClose, onSave }) => {
 
   const validateForm = () => {
     const newErrors = {}
-    if (!formData.nombre.trim()) {
+    if (!formData.nombre.trim() || formData.nombre.length==0) {
       newErrors.nombre = "El nombre es obligatorio"
     } else if (formData.nombre.trim().length < 3) {
       newErrors.nombre = "El nombre debe tener al menos 3 caracteres"
